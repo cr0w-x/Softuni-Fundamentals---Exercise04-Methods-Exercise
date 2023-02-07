b23856c7b6e33e19ac40bd7381525a2456c78286
cr0w- <cr0w-@MILEN-PC>
2023-02-07 16:46:12 +0200

    sync

---
+++ 03. Characters in Range/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            CharsInRange(a, b);
+        }
+
+        private static void CharsInRange(char firstChar, char secondChar)
+        {
+            if (firstChar < secondChar)
+            {
+                for (char a = (char)(firstChar + 1); a < secondChar; a++)
+                {
+                    Console.Write(a + " ");
+                }
+            }
+            else
+            {
+                for (char a = (char)(secondChar + 1); a < firstChar; a++)
+                {
+                    Console.Write(a + " ");
+                }
+            }
+        }
+    }
+}
