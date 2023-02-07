ac38786bcd091c9b3d6a8c8b3c4950f8598d7ed2
cr0w- <cr0w-@MILEN-PC>
2023-02-07 15:59:13 +0200

    sync

---
+++ 01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {            
+            Smallest();
+        }
+
+        static void Smallest()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            int[] funcArray= { a, b, c };
+            int smallest = a;
+
+            for (int i = 0; i < funcArray.Length; i++)
+            {
+                for (int j = i+1; j < funcArray.Length; j++)
+                {
+                    if (funcArray[j] < smallest)
+                    {
+                        smallest = funcArray[j];
+                    }
+                }
+            }
+            Console.WriteLine(smallest);
+        }
+    }
+}
+
+
