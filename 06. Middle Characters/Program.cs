5829bf295d5f3ea8bcc45ccf2c2ee4bd7c527714
cr0w-x <cr0w-x@MILEN-PC>
2023-02-18 10:36:06 +0200

    sync

---
+++ 06. Middle Characters/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Console.WriteLine(MiddleTwo(input));
+        }
+        static string MiddleTwo(string input)
+        {
+            char[]chars = input.ToCharArray();
+            int counter = 0;
+            bool ifEven = false;
+            var middleChars = new List<string>();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                counter++;
+            }
+            if (counter %2 == 0)
+            {
+                ifEven = true;
+            }
+            if (ifEven)
+            {
+                middleChars.Add(chars[counter / 2 - 1].ToString());
+                middleChars.Add(chars[counter / 2].ToString());
+            }
+            else
+            {
+                middleChars.Add(chars[counter / 2].ToString());
+            }
+            return (string.Join("",middleChars));
+        }
+    }
+}
