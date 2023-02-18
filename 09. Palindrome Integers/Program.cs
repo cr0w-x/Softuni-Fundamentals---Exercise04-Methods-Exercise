e5273770d5405c4335461ad73c5a787e68e5f3db
cr0w-x <cr0w-x@MILEN-PC>
2023-02-18 11:20:10 +0200

    sync

---
+++ 09. Palindrome Integers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                if (IsPalindromeOrNot(input))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    Console.WriteLine("false");
+                }
+            }
+
+            static bool IsPalindromeOrNot(string input)
+            {
+                string reverceString = string.Empty;
+
+                for (int i = input.Length - 1; i >= 0; i--)
+                {
+                    reverceString += input[i];
+                }
+
+                if (input == reverceString)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
