e4eebb425bde9d59c26ae70dff750cd7041fe3e1
cr0w-x <cr0w-x@MILEN-PC>
2023-02-18 11:08:00 +0200

    sync

---
+++ 08. Factorial Division/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            decimal factorialDivision = (decimal)Factorial(a) / (decimal)Factorial(b);
+            Console.WriteLine($"{factorialDivision:f2}");
+        }
+        static long Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * (Factorial(n - 1));
+            }
+        }
+    }
+}
