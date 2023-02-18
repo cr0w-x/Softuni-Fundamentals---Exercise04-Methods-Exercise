ff353f4ac420907956e16af8234ac387923b7fa5
cr0w-x <cr0w-x@MILEN-PC>
2023-02-18 10:08:59 +0200

    sync

--- 05. Add and Subtract/Program.cs
+++ 05. Add and Subtract/Program.cs
@@ -6,7 +6,19 @@ namespace _05._Add_and_Subtract
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(SubtractNum(SumOfTwo(a,b),c));
+        }
+        static int SumOfTwo(int a, int b)
+        {
+            return a + b;
+        }
+        static int SubtractNum(int a, int b)
+        {
+            return a - b;
         }
     }
 }
